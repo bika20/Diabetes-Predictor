cbf4075084d56a9c804c352b1e0b79237fc88478
MeSanskarSingh <sanskar.singh.168@gmail.com>
2026-03-25 04:44:28 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -43,7 +43,7 @@ export default function Home() {
     };
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/predict", {
+      const response = await fetch("https://diabetes-predictor-mjpi.onrender.com/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
